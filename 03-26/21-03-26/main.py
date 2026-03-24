da7552867d01bd1549947b87f70c653dd645a72b
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-03-24 05:40:56 -0300

    Busca de tarefas por ID via API

--- 03-26/21-03-26/main.py
+++ 03-26/21-03-26/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import sys
 sys.path.append('/mnt/1962bfc0-f9ca-4c35-a3e2-7f1d23b699bd/PROGRAMAS/Cursos/Python/Estudos/03-26/14-03-26')
 from manager import TaskManager
@@ -15,3 +15,11 @@ def home():
 def listar_tarefas_api():
     manager.task_list = manager.load_task()
     return manager.task_list
+
+@app.get("/tarefas/{id}")
+def listtask_fromid(id: str):
+    tarefas = manager.load_task()
+    tarefa_encontrada = next((task for task in tarefas if task["ID"] == id), None)
+    if not tarefa_encontrada:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
+    return tarefa_encontrada
