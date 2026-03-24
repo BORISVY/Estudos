b46516abf210d3e3e6f2b7203ee1f8fb12a1a241
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-03-24 05:16:25 -0300

    Criando minha primeira API

--- 03-26/21-03-26/main.py
+++ 03-26/21-03-26/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import sys
+sys.path.append('/mnt/1962bfc0-f9ca-4c35-a3e2-7f1d23b699bd/PROGRAMAS/Cursos/Python/Estudos/03-26/14-03-26')
+from manager import TaskManager
 
 app = FastAPI()
 
+manager = TaskManager()
+
 @app.get("/")
 def home():
-    return{"Mensagem": "Minha Primeira API"}
+    return {"status": "API Online", "sistema": "Gerenciador de Tarefas"}
+
+@app.get("/tarefas")
+def listar_tarefas_api():
+    manager.task_list = manager.load_task()
+    return manager.task_list
