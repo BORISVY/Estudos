7055a0ddf77099f117cefd5d912a58a97b16202e
João Pedro Bergamini <joaopedrobergamini16062000@gmail.com>
2026-03-25 05:52:18 -0300

    A API está funcionando para criar novas tarefas, listar e consultar via ID

--- 03-26/21-03-26/main.py
+++ 03-26/21-03-26/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 import sys
+
 sys.path.append('/mnt/1962bfc0-f9ca-4c35-a3e2-7f1d23b699bd/PROGRAMAS/Cursos/Python/Estudos/03-26/14-03-26')
 from manager import TaskManager
 
+class TaskRequest(BaseModel):
+    title: str
+    desc: Optional[str] = ""
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 manager = TaskManager()
 
@@ -23,3 +38,11 @@ def listtask_fromid(id: str):
     if not tarefa_encontrada:
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
     return tarefa_encontrada
+
+@app.post("/tarefas", status_code=201)
+async def create_task(task_data: TaskRequest):
+    try:
+        manager.new_task(task_data.title, task_data.desc)
+        return {"message": "Tarefa criada!", "all_tasks": manager.task_list}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
